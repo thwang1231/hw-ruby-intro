821aa7ae1436ece561e390c096c2dee8dca0e0e2
Guacore <thwang1231@gmail.com>
2019-09-19 18:37:14 -0500

    HW1 submission

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -17,37 +17,99 @@ def max_2_sum arr
     elsif arr.size == 1
         return arr[0]
     else
-        puts "sorted arrasdfsadfsdfasdfasddfsasd"
-        puts arr.sort
-        puts "largestlargestlargest"
-        k = arr.sort
-        puts k[arr.size]
-        puts "endendendend"
-        return 0
+        sorted = arr.sort
+        size = sorted.size
+        return sorted[size-1] + sorted[size-2]
     end
 
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+    max_idx = arr.size-1
+
+    for i in 0 .. max_idx do
+        for j in 0 .. max_idx do
+            if i != j and arr[i]+arr[j] == n
+                return true
+            end
+        end 
+    end
+
+    return false
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+
+    return "Hello, " + name
+
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+
+    if s==""
+        return false
+    end
+
+    down_s = s.downcase
+    if "bcdfghjklmnpqrstvwxyz".include? down_s[0]
+        return true
+    end
+
+    return false
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+
+    if s == "0"
+        return true
+    end
+
+    size = s.size
+    if s[size-1] == "0" and s[size-2] == "0" and s.scan(/\D/).empty?
+            return true
+    end
+
+    return false
+
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+    def initialize(isbn, price)
+        if isbn == "" or price <= 0
+            raise ArgumentError.new("No ISBN or non-positive price")
+        end
+
+        @isbn = isbn
+        @price = price
+    end
+
+    def isbn
+        @isbn
+    end
+
+    def price
+        @price
+    end
+
+    def isbn=(isbn)
+        @isbn = isbn
+    end
+
+    def price=(price)
+        @price = price
+    end
+
+    def price_as_string()
+        return "$%0.2f" % [@price]
+    end
+
 end
